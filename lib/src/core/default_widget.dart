702be6a69bc7ecaf769dc7db866a5f129cc98a25
Monika <yuju.li@jibinfo.com.cn>
2022-01-20 09:30:51 +0800

    优化for循环

--- lib/src/core/default_widget.dart
+++ lib/src/core/default_widget.dart
@@ -2,56 +2,32 @@ part of 'water_mark.dart';
 
 List<Widget> buildMarkListWidget(widget,
     {rowCount, columnCount, textAlign, textStyle, angle}) {
-  List<Widget> list = [];
-  for (var i = 0; i < rowCount; i++) {
-    Widget child = Expanded(
-        child: Row(
-      children:
-          buildItem(widget: widget, columnCount: columnCount, angle: angle),
-    ));
-    list.add(child);
-  }
-  return list;
+  return List.filled(
+      rowCount,
+      Expanded(
+          child: Row(
+        children:
+            buildItem(widget: widget, columnCount: columnCount, angle: angle),
+      )));
 }
 
-/// 原方法来源
-/// @description: water mark can used in page for security .
-/// @author: weichunsheng@jd.com
-/// @date: 2020/8/31 11:44 AM
-/// @version: 1.0
-/// @from: https://github.com/wei-spring/water_mark
-/// @bloc:https://www.cnblogs.com/spring87/p/13597775.html
-///
-///
 List<Widget> buildMarkList(msg,
     {rowCount, columnCount, textAlign, textStyle, angle, softWrap}) {
-  List<Widget> list = [];
-  for (var i = 0; i < rowCount; i++) {
-    Widget widget = Expanded(
-        child: Row(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: buildItem(
-          text: msg,
-          columnCount: columnCount,
-          textAlign: textAlign,
-          textStyle: textStyle,
-          angle: angle,
-          softWrap: softWrap),
-    ));
-    list.add(widget);
-  }
-  return list;
+  return List.filled(
+      rowCount,
+      Expanded(
+          child: Row(
+        children: List.filled(
+            columnCount,
+            itemView(
+                text: msg,
+                textAlign: textAlign,
+                textStyle: textStyle,
+                angle: angle,
+                softWrap: softWrap)),
+      )));
 }
 
-/// 原方法来源
-/// @description: water mark can used in page for security .
-/// @author: weichunsheng@jd.com
-/// @date: 2020/8/31 11:44 AM
-/// @version: 1.0
-/// @from: https://github.com/wei-spring/water_mark
-/// @bloc:https://www.cnblogs.com/spring87/p/13597775.html
-///
 List<Widget> buildItem(
     {text,
     widget,
@@ -60,37 +36,43 @@ List<Widget> buildItem(
     textStyle,
     angle,
     softWrap}) {
-  List<Widget> list = [];
-  for (var i = 0; i < columnCount; i++) {
-    Widget child = Expanded(
-      child: Center(
-        child: MeasureSize(
-          child: Transform.rotate(
-            angle: angle ?? (-pi / 12),
-            child: text != null
-                ? Text(
-                    text,
-                    textAlign: textAlign,
-                    style: textStyle,
-                    softWrap: false, //默认不换行
-                  )
-                : widget ?? const SizedBox(),
-          ),
-          onChange: (size) {
-            //if (screenSize != null && size != null) {
-            //todo
-            //如果有需要动态计算水印个数，可以通过屏幕宽高，以及单个水印Widget的宽高，
-            // 以及水印间距，算出需要展示水印个数,然后动态展示，
-            // 别忘了把Widget换成有状态的动态刷新
-            //eg:间距为0个数
-            //rowCount = (screenSize.width ~/ size.width) ;
-            //columnCount = (screenSize.height ~/ size.height);
-            //}
-          },
-        ),
-      ),
-    );
-    list.add(child);
-  }
-  return list;
+  return List.filled(
+      columnCount,
+      itemView(
+          text: text,
+          textAlign: textAlign,
+          textStyle: textStyle,
+          angle: angle,
+          softWrap: softWrap,
+          widget: widget));
+}
+
+Expanded itemView({angle, text, textAlign, textStyle, widget, softWrap}) {
+  return Expanded(
+      child: MeasureSize(
+    child: Transform.rotate(
+      angle: angle ?? (-pi / 12),
+      child: text != null
+          ? Center(
+              child: Text(
+                text,
+                textAlign: textAlign,
+                style: textStyle,
+                softWrap: softWrap, //默认不换行
+              ),
+            )
+          : widget ?? const SizedBox(),
+    ),
+    onChange: (size) {
+      //if (screenSize != null && size != null) {
+      //todo
+      //如果有需要动态计算水印个数，可以通过屏幕宽高，以及单个水印Widget的宽高，
+      // 以及水印间距，算出需要展示水印个数,然后动态展示，
+      // 别忘了把Widget换成有状态的动态刷新
+      //eg:间距为0个数
+      //rowCount = (screenSize.width ~/ size.width) ;
+      //columnCount = (screenSize.height ~/ size.height);
+      //}
+    },
+  ));
 }
